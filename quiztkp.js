ae447ebd8903335c2a93e0defdf7285c857ac6e7
Latihan101 <yikapps98@gmail.com>
2026-06-13 00:58:29 +0700

    Create quiztkp.js

---
+++ quiztkp.js
@@ -0,0 +1,272 @@
+var quizTKP= [
+  { // No 66
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,1,5,2,3],
+    pembahasan: ""
+  },
+  { // No 67
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,1,2,5,4],
+    pembahasan: ""
+  },
+  { // No 68
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,2,5,1,4],
+    pembahasan: ""
+  },
+  { // No 69
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,2,5,1,4],
+    pembahasan: ""
+  },
+  { // No 70
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,2,1,3,5],
+    pembahasan: ""
+  },
+  { // No 71
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,4,2,1,5],
+    pembahasan: ""
+  },
+  { // No 72
+    q: "",
+    opts: ["","","","",""],
+    scores: [1,4,5,3,2],
+    pembahasan: ""
+  },
+  { // No 73
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,5,1,3,2],
+    pembahasan: ""
+  },
+  { // No 74
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,1,3,5,2],
+    pembahasan: ""
+  },
+  { // No 75
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,5,1,4,3],
+    pembahasan: ""
+  },
+  { // No 76
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,1,3,5,4],
+    pembahasan: ""
+  },
+  { // No 77
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,3,1,5,2],
+    pembahasan: ""
+  },
+  { // No 78
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,4,5,1,3],
+    pembahasan: ""
+  },
+  { // No 79
+    q: "",
+    opts: ["","","","",""],
+    scores: [1,2,4,3,5],
+    pembahasan: ""
+  },
+  { // No 80
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,1,5,2,4],
+    pembahasan: ""
+  },
+  { // No 81
+    q: "",
+    opts: ["","","","",""],
+    scores: [1,4,3,5,2],
+    pembahasan: ""
+  },
+  { // No 82
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,2,4,1,3],
+    pembahasan: ""
+  },
+  { // No 83
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,1,3,5,2],
+    pembahasan: ""
+  },
+  { // No 84
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,1,3,4,2],
+    pembahasan: ""
+  },
+  { // No 85
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,4,1,2,5],
+    pembahasan: ""
+  },
+  { // No 86
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,5,4,1,3],
+    pembahasan: ""
+  },
+  { // No 87
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,5,2,4,1],
+    pembahasan: ""
+  },
+  { // No 88
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,3,1,2,4],
+    pembahasan: ""
+  },
+  { // No 89
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,5,1,3,2],
+    pembahasan: ""
+  },
+  { // No 90
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,4,1,5,2],
+    pembahasan: ""
+  },
+  { // No 91
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,2,1,5,3],
+    pembahasan: ""
+  },
+  { // No 92
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,1,4,2,3],
+    pembahasan: ""
+  },
+  { // No 93
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,1,5,3,2],
+    pembahasan: ""
+  },
+  { // No 94
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,2,3,1,5],
+    pembahasan: ""
+  },
+  { // No 95
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,4,3,2,1],
+    pembahasan: ""
+  },
+  { // No 96
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,4,1,2,5],
+    pembahasan: ""
+  },
+  { // No 97
+    q: "",
+    opts: ["","","","",""],
+    scores: [1,3,2,5,4],
+    pembahasan: ""
+  },
+  { // No 98
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,5,1,3,2],
+    pembahasan: ""
+  },
+  { // No 99
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,5,3,4,1],
+    pembahasan: ""
+  },
+  { // No 100
+    q: "",
+    opts: ["","","","",""],
+    scores: [1,5,2,3,4],
+    pembahasan: ""
+  },
+  { // No 101
+    q: "",
+    opts: ["","","","",""],
+    scores: [5,4,1,3,2],
+    pembahasan: ""
+  },
+  { // No 102
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,2,1,5,3],
+    pembahasan: ""
+  },
+  { // No 103
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,3,5,1,4],
+    pembahasan: ""
+  },
+  { // No 104
+    q: "",
+    opts: ["","","","",""],
+    scores: [2,3,4,5,1],
+    pembahasan: ""
+  },
+  { // No 105
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,2,4,5,1],
+    pembahasan: ""
+  },
+  { // No 106
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,1,5,2,3],
+    pembahasan: ""
+  },
+  { // No 107
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,1,2,5,4],
+    pembahasan: ""
+  },
+  { // No 108
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,2,5,1,4],
+    pembahasan: ""
+  },
+  { // No 109
+    q: "",
+    opts: ["","","","",""],
+    scores: [3,2,5,1,4],
+    pembahasan: ""
+  },
+  { // No 110
+    q: "",
+    opts: ["","","","",""],
+    scores: [4,2,1,3,5],
+    pembahasan: ""
+  }
+  ];
